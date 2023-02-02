c0333a32bf0a2f166eb334e9ac64f806fdca3c0b
Arun Kumar <112628113+Akgoldie@users.noreply.github.com>
2023-02-02 23:34:51 +0530

    Number Guessing Game

---
+++ Project_3.py
@@ -0,0 +1,22 @@
+import random
+
+def guess(num,guess_num):
+    if(num==guess_num):
+        print("Cool guessed number is right! ")
+    else:
+        print("you guessed number is wrong! ")
+        print("If you want to continue press 1 or press 0")
+        con=int(input())
+        if(con==1):
+            guess_num = int(input("Enter the guessed number: "))
+            guess(num, guess_num)
+        else:
+            print("Bye, Have a good day")
+
+
+
+num= random.randrange(0,9)                               #Generate a random number between(0 to 9)
+guess_num= int(input("Enter the guessed number: "))      #entered number
+guess(num, guess_num)
+
+
